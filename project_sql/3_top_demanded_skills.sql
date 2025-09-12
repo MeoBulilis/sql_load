cddd305a3e198518b22fcb19380f6a4220a4122b
Axeom30 <romeobulilis@gmail.com>
2025-09-12 18:06:36 +0800

    Practice Problem 2

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,21 @@
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title
+    FROM 
+        job_postings_fact
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere'
+)
+
+SELECT 
+    skills_dim.skill,
+    Count(DISTINCT top_paying_jobs.job_id) AS skill_count
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    skills_dim.skill
+ORDER BY
+    skill_count DESC;
